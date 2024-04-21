25cf0527fccfc76575f6abb5ff1c623e8ab58f26
Masoud <masoud.zeinalabedini@gmail.com>
2024-04-21 15:25:42 +0330

    set up SnippetCreatePage

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,7 +1,37 @@
 interface Props {}
 
 const SnippetCreatePage = (props: Props) => {
-  return <div>SnippetCreatePage</div>;
+  return (
+    <form>
+      <h3 className="font-bold m-3">Crete Snippet </h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="title">
+            Title:
+          </label>
+          <input
+            type="text"
+            className="border rounded w-full p-2"
+            name="title"
+            id="title"
+          />
+        </div>
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="code">
+            Code:
+          </label>
+          <textarea
+            className="border rounded w-full p-2"
+            name="code"
+            id="code"
+          />
+        </div>
+        <button type="submit" className="rounded bg-blue-300 p-2">
+          Create
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export default SnippetCreatePage;
